a770922dadbf0ad900d34a21a2844d85c2b84766
LIGHTJ60 <jligh43@wgu.edu>
2023-11-10 16:49:15 -0700

    More setting up

--- app/src/main/java/com/example/vacationscheduler/UI/MainActivity.java
+++ app/src/main/java/com/example/vacationscheduler/UI/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.vacationscheduler;
+package com.example.vacationscheduler.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.vacationscheduler.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/vacationscheduler/UI/VacationDetails.java
@@ -0,0 +1,16 @@
+package com.example.vacationscheduler.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vacationscheduler.R;
+
+public class VacationDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vacation_details);
+    }
+}
