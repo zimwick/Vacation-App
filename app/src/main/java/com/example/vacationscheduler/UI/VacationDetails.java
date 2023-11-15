c5e388d414a302bf870185e06b099dda3340c70e
LIGHTJ60 <jligh43@wgu.edu>
2023-11-15 15:13:56 -0700

    Added excursions entry

--- app/src/main/java/com/example/vacationscheduler/UI/VacationDetails.java
+++ app/src/main/java/com/example/vacationscheduler/UI/VacationDetails.java
@@ -2,9 +2,12 @@ package com.example.vacationscheduler.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.vacationscheduler.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class VacationDetails extends AppCompatActivity {
 
@@ -12,5 +15,14 @@ public class VacationDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vacation_details);
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton2);
+        fab.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(VacationDetails.this, ExcursionDetails.class);
+                startActivity(intent);
+            }
+        });
     }
 }
