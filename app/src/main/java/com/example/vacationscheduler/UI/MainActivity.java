20d7886adabbacabda9f695bfbc077571894b51f
LIGHTJ60 <jligh43@wgu.edu>
2023-11-15 12:03:45 -0700

    B1. Complete

--- app/src/main/java/com/example/vacationscheduler/UI/MainActivity.java
+++ app/src/main/java/com/example/vacationscheduler/UI/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.vacationscheduler.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.vacationscheduler.R;
 
@@ -12,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, VacationList.class);
+                intent.putExtra("test", "Information sent");
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/vacationscheduler/UI/VacationList.java
+++ app/src/main/java/com/example/vacationscheduler/UI/VacationList.java
@@ -2,9 +2,15 @@ package com.example.vacationscheduler.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.vacationscheduler.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class VacationList extends AppCompatActivity {
 
@@ -12,5 +18,33 @@ public class VacationList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vacation_list);
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(VacationList.this, VacationDetails.class);
+                startActivity(intent);
+            }
+        });
+
+        System.out.println(getIntent().getStringExtra("test"));
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_vacation_list, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId()==R.id.sample){
+            Toast.makeText(VacationList.this, "put in sample data", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if(item.getItemId()==android.R.id.home){
+            this.finish();
+            return true;
+        }
+        return true;
     }
 }
