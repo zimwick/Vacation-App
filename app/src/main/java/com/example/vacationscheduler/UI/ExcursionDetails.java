cea8da7bd84664c3f06f632a7c6c2bc01f56b00f
LIGHTJ60 <jligh43@wgu.edu>
2023-11-16 16:41:29 -0700

    new interfaces

--- app/src/main/java/com/example/vacationscheduler/UI/ExcursionDetails.java
+++ app/src/main/java/com/example/vacationscheduler/UI/ExcursionDetails.java
@@ -3,14 +3,35 @@ package com.example.vacationscheduler.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.vacationscheduler.R;
+import com.example.vacationscheduler.database.Repository;
 
 public class ExcursionDetails extends AppCompatActivity {
+
+    String title;
+    String date;
+    int excursionID;
+    int vacationID;
+    EditText editTitle;
+    EditText editDate;
+    Repository repository;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_excursion_details);
+        repository = new Repository(getApplication());
+        title = getIntent().getStringExtra("Title");
+        editTitle = findViewById(R.id.extitletext);
+        editTitle.setText(title);
+        date = getIntent().getStringExtra("Date");
+        editDate = findViewById(R.id.exdatetext);
+        editDate.setText(date);
+        excursionID = getIntent().getIntExtra("ID", -1);
+        vacationID = getIntent().getIntExtra("vacID", -1);
+
     }
 }
